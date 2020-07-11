39bdd55301def569c43d6848a5252e91b90e4d3d
Derek Borski <derek.borski@gmail.com>
2020-07-11 12:21:37 -0600

    Add depdendent destroy for relationships

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,5 @@
 class Customer < ApplicationRecord
-  has_many :invoices
+  has_many :invoices, dependent: :destroy
   has_many :merchants, through: :invoices
 
   validates_presence_of :first_name,
