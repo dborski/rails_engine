a320e05db7078474c6ee48d1953fc999324bd76f
Derek Borski <derek.borski@gmail.com>
2020-07-11 18:46:18 -0600

    Update index request spec

--- spec/requests/api/v1/merchant_request_spec.rb
+++ spec/requests/api/v1/merchant_request_spec.rb
@@ -3,13 +3,13 @@ require 'rails_helper'
 describe "Merchants API" do
   it "sends a list of merchants" do
     create_list(:merchant, 5)
-
+    
     get '/api/v1/merchants'
 
     expect(response).to be_successful
 
     merchants = JSON.parse(response.body)
 
-    expect(merchants.count).to eq(5)
+    expect(merchants["data"].count).to eq(5)
   end
 end
