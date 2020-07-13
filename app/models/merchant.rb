8430ec441120e21a3f8df8c6ddcc766f961072fc
Derek Borski <derek.borski@gmail.com>
2020-07-13 01:36:46 -0600

    Add scoped search function for finding one merchant and finding all merchants that meet parameters

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,4 +4,32 @@ class Merchant < ApplicationRecord
   has_many :customers, through: :invoices
 
   validates_presence_of :name
+
+  def self.find_one(name = nil, created_at = nil, updated_at = nil)
+    self.find_all(name, created_at, updated_at).first
+  end 
+
+  def self.find_all(name = nil, created_at = nil, updated_at = nil)
+    with_name(name)
+      .with_created_at(created_at)
+      .with_updated_at(updated_at)
+  end
+
+  scope :with_name, proc { |name| 
+    if name.present?
+      where("name ILIKE ?", "%#{name}%")
+    end 
+  }
+
+  scope :with_created_at, proc { |created_at| 
+    if created_at.present?
+      where("to_char(created_at,'yyyy-mm-dd-HH-MI-SS') ILIKE ?", "%#{created_at}%")
+    end 
+  }
+
+  scope :with_updated_at, proc { |updated_at| 
+    if updated_at.present?
+      where("to_char(updated_at,'yyyy-mm-dd-HH-MI-SS') ILIKE ?", "%#{updated_at}%")
+    end 
+  }
 end
