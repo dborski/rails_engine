d9df741c681a666dfa12d18e99fbc8407bd0209b
Derek Borski <derek.borski@gmail.com>
2020-07-13 08:34:14 -0600

    Add specs for merchant mult finder endpoints

--- spec/requests/api/v1/search_endpoints/merchant_search_spec.rb
+++ spec/requests/api/v1/search_endpoints/merchant_search_spec.rb
@@ -15,49 +15,127 @@ describe 'Merchants API' do
     expect(merchant['data']['attributes']['id']).to eq(@merchant1.id)
     expect(merchant['data']['attributes']['name']).to eq(@merchant1.name)
   end
-
+  
   it 'can find a merchant that has a fragment in created_at' do
     get "/api/v1/merchants/find?created_at=2019"
-
+    
     merchant = JSON.parse(response.body)
-
+    
     expect(merchant['data']['attributes']['id']).to eq(@merchant2.id)
     expect(merchant['data']['attributes']['name']).to eq(@merchant2.name)
   end
-
+  
   it 'can find a merchant that has a fragment in updated_at' do
     get "/api/v1/merchants/find?updated_at=19"
-
+    
     merchant = JSON.parse(response.body)
-
+    
     expect(merchant['data']['attributes']['id']).to eq(@merchant2.id)
     expect(merchant['data']['attributes']['name']).to eq(@merchant2.name)
   end
   
   it 'can find a merchant that has a fragment in both name and created_at' do
     get "/api/v1/merchants/find?name=pu&updated_at=19"
-
+    
     merchant = JSON.parse(response.body)
-
+    
     expect(merchant['data']['attributes']['id']).to eq(@merchant2.id)
     expect(merchant['data']['attributes']['name']).to eq(@merchant2.name)
   end
-
+  
   it 'can find a merchant that has a fragment in both name and updated_at' do
     get "/api/v1/merchants/find?name=ar&updated_at=18"
-
+    
     merchant = JSON.parse(response.body)
-
+    
+    expect(merchant['data']['attributes']['id']).to eq(@merchant3.id)
+    expect(merchant['data']['attributes']['name']).to eq(@merchant3.name)
+  end
+  
+  it 'can find a merchant that has a fragment in both created_at and updated_at' do
+    get "/api/v1/merchants/find?created_at=dec&updated_at=18"
+    
+    merchant = JSON.parse(response.body)
+    
     expect(merchant['data']['attributes']['id']).to eq(@merchant3.id)
     expect(merchant['data']['attributes']['name']).to eq(@merchant3.name)
   end
 
-  it 'can find a merchant that has a fragment in both created_at and updated_at' do
-    get "/api/v1/merchants/find?created_at=dec&updated_at=18"
+  it 'can find multiple merchants that have a fragment in name' do
+    get "/api/v1/merchants/find_all?name=uT"
 
-    merchant = JSON.parse(response.body)
+    merchants = JSON.parse(response.body)
 
-    expect(merchant['data']['attributes']['id']).to eq(@merchant3.id)
-    expect(merchant['data']['attributes']['name']).to eq(@merchant3.name)
+    found_merchants = merchants['data']
+
+    expect(found_merchants.first['attributes']['id']).to eq(@merchant2.id)
+    expect(found_merchants.first['attributes']['name']).to eq(@merchant2.name)
+    expect(found_merchants.last['attributes']['id']).to eq(@merchant3.id)
+    expect(found_merchants.last['attributes']['name']).to eq(@merchant3.name)
+  end
+
+  it 'can find multiple merchants that have a fragment in created_at' do
+    get "/api/v1/merchants/find_all?created_at=19"
+
+    merchants = JSON.parse(response.body)
+
+    found_merchants = merchants['data']
+
+    expect(found_merchants.first['attributes']['id']).to eq(@merchant2.id)
+    expect(found_merchants.first['attributes']['name']).to eq(@merchant2.name)
+    expect(found_merchants.last['attributes']['id']).to eq(@merchant3.id)
+    expect(found_merchants.last['attributes']['name']).to eq(@merchant3.name)
+  end
+
+  it 'can find multiple merchants that have a fragment in updated_at' do
+    get "/api/v1/merchants/find_all?updated_at=17"
+
+    merchants = JSON.parse(response.body)
+
+    found_merchants = merchants['data']
+
+    expect(found_merchants.first['attributes']['id']).to eq(@merchant2.id)
+    expect(found_merchants.first['attributes']['name']).to eq(@merchant2.name)
+    expect(found_merchants.last['attributes']['id']).to eq(@merchant3.id)
+    expect(found_merchants.last['attributes']['name']).to eq(@merchant3.name)
+  end
+
+  it 'can find multiple merchants that have a fragment in name and created_at' do
+    get "/api/v1/merchants/find_all?name=ut&created_at=19"
+
+    merchants = JSON.parse(response.body)
+
+    found_merchants = merchants['data']
+
+    expect(found_merchants.first['attributes']['id']).to eq(@merchant2.id)
+    expect(found_merchants.first['attributes']['name']).to eq(@merchant2.name)
+    expect(found_merchants.last['attributes']['id']).to eq(@merchant3.id)
+    expect(found_merchants.last['attributes']['name']).to eq(@merchant3.name)
+  end
+
+  it 'can find multiple merchants that have a fragment in name and updated_at' do
+    get "/api/v1/merchants/find_all?name=ut&updated_at=17"
+
+    merchants = JSON.parse(response.body)
+
+    found_merchants = merchants['data']
+
+    expect(found_merchants.first['attributes']['id']).to eq(@merchant2.id)
+    expect(found_merchants.first['attributes']['name']).to eq(@merchant2.name)
+    expect(found_merchants.last['attributes']['id']).to eq(@merchant3.id)
+    expect(found_merchants.last['attributes']['name']).to eq(@merchant3.name)
+  end
+
+  it 'can find multiple merchants that have a fragment in created_at and updated_at' do
+    get "/api/v1/merchants/find_all?created_at=19&updated_at=17"
+
+    merchants = JSON.parse(response.body)
+
+    found_merchants = merchants['data']
+
+    expect(found_merchants.first['attributes']['id']).to eq(@merchant2.id)
+    expect(found_merchants.first['attributes']['name']).to eq(@merchant2.name)
+    expect(found_merchants.last['attributes']['id']).to eq(@merchant3.id)
+    expect(found_merchants.last['attributes']['name']).to eq(@merchant3.name)
   end
 end
