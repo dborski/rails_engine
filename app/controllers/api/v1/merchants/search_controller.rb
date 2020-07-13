50a11c32021ceed978cd68af22c98e11a91e931f
Derek Borski <derek.borski@gmail.com>
2020-07-13 08:34:34 -0600

    Add index action

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -1,4 +1,9 @@
 class Api::V1::Merchants::SearchController < ActionController::API
+  def index
+    merchants = Merchant.find_all(params[:name], params[:created_at], params[:updated_at])
+    render json: MerchantSerializer.new(merchants)
+  end 
+
   def show
     merchant = Merchant.find_one(params[:name], params[:created_at], params[:updated_at])
     render json: MerchantSerializer.new(merchant)
