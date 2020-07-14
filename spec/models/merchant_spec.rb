bbd74ebdfefbf3464d585bda6d3ce45f54183501
Derek Borski <derek.borski@gmail.com>
2020-07-13 21:30:48 -0600

    Update merchants_by_revenue spec

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -21,21 +21,21 @@ RSpec.describe Merchant, type: :model do
       @item2 = create(:item, merchant: @merchant1, unit_price: 20.55)
       @item3 = create(:item, merchant: @merchant2, unit_price: 99.99)
       @item4 = create(:item, merchant: @merchant2, unit_price: 5.50)
-      @item5 = create(:item, merchant: @merchant3, unit_price: 15.30)
+      @item5 = create(:item, merchant: @merchant3, unit_price: 16.30)
 
       @invoice1 = create(:invoice, merchant: @merchant1)
       @invoice2 = create(:invoice, merchant: @merchant1)
       @invoice3 = create(:invoice, merchant: @merchant2)
       @invoice4 = create(:invoice, merchant: @merchant2)
       @invoice5 = create(:invoice, merchant: @merchant3)
-      @invoice6 = create(:invoice, merchant: @merchant3, status: 'on hold')
+      @invoice6 = create(:invoice, merchant: @merchant3)
       
       @invoice_item1 = create(:invoice_item, item: @item1, invoice: @invoice1, quantity: 1, unit_price: 10)
       @invoice_item2 = create(:invoice_item, item: @item2, invoice: @invoice1, quantity: 1, unit_price: 20.55)
       @invoice_item3 = create(:invoice_item, item: @item3, invoice: @invoice2, quantity: 1, unit_price: 99.99)
       @invoice_item4 = create(:invoice_item, item: @item4, invoice: @invoice3, quantity: 1, unit_price: 5.50)
       @invoice_item4 = create(:invoice_item, item: @item5, invoice: @invoice4, quantity: 1, unit_price: 15.30)
-      @invoice_item4 = create(:invoice_item, item: @item5, invoice: @invoice5, quantity: 1, unit_price: 15.30)
+      @invoice_item4 = create(:invoice_item, item: @item5, invoice: @invoice5, quantity: 1, unit_price: 16.30)
 
       @transaction1 = create(:transaction, invoice: @invoice1)
       @transaction2 = create(:transaction, invoice: @invoice2)
@@ -45,7 +45,7 @@ RSpec.describe Merchant, type: :model do
     end
 
     it 'can find merchants by revenue' do
-      expect(Merchant.merchants_by_revenue(2, 'desc')).to eq([@merchant1, @merchant2])
+      expect(Merchant.merchants_by_revenue(2, 'desc')).to eq([@merchant1, @merchant3])
     end 
   end
 end
