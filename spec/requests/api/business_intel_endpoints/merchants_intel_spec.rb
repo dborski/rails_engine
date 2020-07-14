867bf316d379e805640e0800fc5bfe572c3a3eca
Derek Borski <derek.borski@gmail.com>
2020-07-13 22:51:00 -0600

    Change name to merchants_intel_spec

--- spec/requests/api/business_intel_endpoints/merchants_intel_spec.rb
+++ spec/requests/api/business_intel_endpoints/merchants_intel_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe 'Business Intelligence APIO' do
+describe 'Business Intelligence API' do
   before(:each) do
     @merchant1 = create(:merchant)
     @merchant2 = create(:merchant)
@@ -23,14 +23,14 @@ describe 'Business Intelligence APIO' do
     @invoice_item2 = create(:invoice_item, item: @item2, invoice: @invoice1, quantity: 1, unit_price: 20.55)
     @invoice_item3 = create(:invoice_item, item: @item3, invoice: @invoice2, quantity: 1, unit_price: 99.99)
     @invoice_item4 = create(:invoice_item, item: @item4, invoice: @invoice3, quantity: 1, unit_price: 5.50)
-    @invoice_item4 = create(:invoice_item, item: @item5, invoice: @invoice4, quantity: 1, unit_price: 15.30)
-    @invoice_item4 = create(:invoice_item, item: @item5, invoice: @invoice5, quantity: 1, unit_price: 15.30)
+    @invoice_item5 = create(:invoice_item, item: @item5, invoice: @invoice4, quantity: 1, unit_price: 15.30)
+    @invoice_item6 = create(:invoice_item, item: @item5, invoice: @invoice5, quantity: 1, unit_price: 15.30)
 
     @transaction1 = create(:transaction, invoice: @invoice1)
     @transaction2 = create(:transaction, invoice: @invoice2)
     @transaction3 = create(:transaction, invoice: @invoice3)
     @transaction4 = create(:transaction, invoice: @invoice4, result: 'failed')
-    @transaction4 = create(:transaction, invoice: @invoice5)
+    @transaction5 = create(:transaction, invoice: @invoice5)
   end
 
   it 'can find 2 merchants with most revenue' do
@@ -42,5 +42,16 @@ describe 'Business Intelligence APIO' do
 
     expect(found_merchants.first['attributes']['id']).to eq(@merchant1.id)
     expect(found_merchants.last['attributes']['id']).to eq(@merchant3.id)
+  end
+
+  it 'can find 2 merchants with most items sold' do
+    get '/api/v1/merchants/most_items?quantity=2'
+
+    merchants = JSON.parse(response.body)
+
+    found_merchants = merchants['data']
+
+    expect(found_merchants.first['attributes']['id']).to eq(@merchant1.id)
+    expect(found_merchants.last['attributes']['id']).to eq(@merchant2.id)
   end
 end
