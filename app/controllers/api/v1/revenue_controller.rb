6419ab794c4d00bc8ef0b756e6f315cd109fcb19
Derek Borski <derek.borski@gmail.com>
2020-07-14 20:44:13 -0600

    Add show action

--- app/controllers/api/v1/revenue_controller.rb
+++ app/controllers/api/v1/revenue_controller.rb
@@ -1,15 +1,28 @@
 class Api::V1::RevenueController < ActionController::API
   def index
-    revenue = Merchant.revenue_by_date_range(params[:start], params[:end])
     json_object = {
         "data": {
           "id": nil,
           "attributes": {
-            "revenue": revenue
+            "revenue": Merchant.revenue_by_date_range(params[:start], params[:end])
           }
         }
       }
 
      render json: json_object
   end
+
+  def show
+    merchant = Merchant.find(params[:merchant_id])
+    json_object = {
+        "data": {
+          "id": nil,
+          "attributes": {
+            "revenue": merchant.invoices.total_revenue
+          }
+        }
+      }
+
+     render json: json_object
+  end 
 end
