7b1a0d713e0ba0b4be4288efd655715de864d655
Derek Borski <derek.borski@gmail.com>
2020-07-14 20:26:20 -0600

    Add render to index action

--- app/controllers/api/v1/revenue_controller.rb
+++ app/controllers/api/v1/revenue_controller.rb
@@ -1,5 +1,15 @@
 class Api::V1::RevenueController < ActionController::API
   def index
     revenue = Merchant.revenue_by_date_range(params[:start], params[:end])
+    json_object = {
+        "data": {
+          "id": nil,
+          "attributes": {
+            "revenue": revenue
+          }
+        }
+      }
+
+     render json: json_object
   end
 end
