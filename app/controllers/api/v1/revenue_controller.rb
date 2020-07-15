72b3bddc4e14aac03ad74fde79d982dff9f915e8
Derek Borski <derek.borski@gmail.com>
2020-07-14 21:25:49 -0600

    DRY up controller by using renderable module to render revenue hashes

--- app/controllers/api/v1/revenue_controller.rb
+++ app/controllers/api/v1/revenue_controller.rb
@@ -1,28 +1,12 @@
 class Api::V1::RevenueController < ActionController::API
-  def index
-    json_object = {
-        "data": {
-          "id": nil,
-          "attributes": {
-            "revenue": Merchant.revenue_by_date_range(params[:start], params[:end])
-          }
-        }
-      }
+  include Renderable
 
-     render json: json_object
+  def index
+    render json: render_revenue(Merchant.revenue_by_date_range(params[:start], params[:end]))
   end
 
   def show
     merchant = Merchant.find(params[:merchant_id])
-    json_object = {
-        "data": {
-          "id": nil,
-          "attributes": {
-            "revenue": merchant.invoices.total_revenue
-          }
-        }
-      }
-
-     render json: json_object
+    render json: render_revenue(merchant.invoices.total_revenue)
   end 
 end
