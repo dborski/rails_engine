1851d1795a3bb9d728c34d00956c5e27b0896e33
Derek Borski <derek.borski@gmail.com>
2020-09-27 00:03:46 -0600

    Create spent_at_merchant class method

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,4 +4,14 @@ class Customer < ApplicationRecord
 
   validates_presence_of :first_name,
                         :last_name
+
+  def self.spent_at_merchant(merchant_id, quantity)
+    self.select("customers.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS total_spent")
+        .joins(invoices: [:invoice_items, :transactions])
+        .where('invoices.merchant_id = ?', "#{merchant_id}")
+        .merge(Transaction.successful)
+        .group(:id)
+        .order('total_spent DESC')
+        .limit("#{quantity}")
+  end
 end
