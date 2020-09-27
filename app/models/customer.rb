643a7d3d4015800a772e153e565b29897eea8526
Derek Borski <derek.borski@gmail.com>
2020-09-27 08:21:38 -0600

    Add default limit to spent_at_merchant method

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,7 +5,7 @@ class Customer < ApplicationRecord
   validates_presence_of :first_name,
                         :last_name
 
-  def self.spent_at_merchant(merchant_id, quantity)
+  def self.spent_at_merchant(merchant_id, quantity = 3)
     self.select("customers.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS total_spent")
         .joins(invoices: [:invoice_items, :transactions])
         .where('invoices.merchant_id = ?', "#{merchant_id}")
